ef84d17fb329f371feac2ff4334eebc909d47de6
Bud_Ton <d2023ujle@gmail.com>
2023-09-14 17:35:22 +0300

    Task No. 4. Field tests

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class Student:
                 for num in row:
                     len += 1
                     sum += num
-            student.total = sum/len
+            student.total = round(sum/len,1)
         else:
             'Error'
 
@@ -36,6 +36,15 @@ class Student:
                f'Курсы в процессе изучения:{self.courses_in_progress}\n'
                f'Завершенные курсы:{self.finished_courses}\n')
         return res
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Not a Student!')
+            return
+        if int(self.total < other.total) == 0:
+            return f'Средний балл у Студента {self.name} {self.surname} больше чем у Студента {other.name} {other.surname}\n'
+        else:
+            return f'Средний балл у Студента {other.name} {other.surname} больше чем у Студента {self.name} {self.surname}\n'
 
 class Mentor:
     def __init__(self, name, surname):
@@ -54,15 +63,23 @@ class Lecturer(Mentor):
                 for num in row:
                     len += 1
                     sum += num
-            lecturer.total = sum/len
-        else:
+            lecturer.total = round(sum/len,1)
             'Error'
 
     def __str__(self):
         res = (f'Имя: {self.name}\n'
                f'Фамилия: {self.surname}\n'
-               f'Средняя оценка за домашние задания: {self.total}\n')
+               f'Средняя оценка за лекции: {self.total}\n')
         return res
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Not a Lecturer!')
+            return
+        if int(self.total < other.total) == 0:
+            return f'Средний балл у Лектора {self.name} {self.surname} больше чем у Лектора {other.name} {other.surname}\n'
+        else:
+            return f'Средний балл у Лектора {other.name} {other.surname} больше чем у Лектора {self.name} {self.surname}\n'
+
 
 
 class Reviewer(Mentor):
@@ -81,37 +98,81 @@ class Reviewer(Mentor):
         return res
 
 
+# СТУДЕНТЫ
+student1 = Student('Максим', 'Федоров', 'мужской')
+student1.courses_in_progress += ['Python']
+student1.courses_in_progress += ['Git']
+student1.finished_courses += ['Знакомство с IT']
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-best_student.courses_in_progress += ['Git']
-best_student.finished_courses += ['Знакомство с IT']
+student2 = Student('Ксения', 'Гусева', 'женский')
+student2.courses_in_progress += ['Python']
+student2.courses_in_progress += ['Git']
+student2.finished_courses += ['Знакомство с IT']
 
 
+# ЛЕКТОРЫ
+lecturer1 = Lecturer('Елена', 'Никитина')
+lecturer1.courses_attached += ['Python']
+lecturer1.courses_attached += ['Git']
 
-some_lecturer = Lecturer('name Lecturer', 'surname Lecture')
-some_lecturer.courses_attached += ['Python']
-some_lecturer.courses_attached += ['Git']
+lecturer2 = Lecturer('Олег', 'Булыгин')
+lecturer2.courses_attached += ['Python']
+lecturer2.courses_attached += ['Git']
+
 # Оценки Лектору от Студента
-best_student.rate_work_lect(some_lecturer, 'Python', 10)
-best_student.rate_work_lect(some_lecturer, 'Git', 8)
+student1.rate_work_lect(lecturer1, 'Python', 5)
+student1.rate_work_lect(lecturer1, 'Python', 9)
+student1.rate_work_lect(lecturer1, 'Python', 8)
+student2.rate_work_lect(lecturer2, 'Git', 8)
+student2.rate_work_lect(lecturer2, 'Git', 9)
+student2.rate_work_lect(lecturer2, 'Git', 8)
 
+# РЕЦЕНЗЕНТ
+reviewer1 = Reviewer('Александр', 'Бардин')
+reviewer1.courses_attached += ['Python']
+reviewer1.courses_attached += ['Git']
 
-some_reviewer = Reviewer('Some', 'Buddy')
-some_reviewer.courses_attached += ['Python']
-some_reviewer.courses_attached += ['Git']
-# Оценки Студенту от Рецензира
-some_reviewer.rate_hw(best_student, 'Python', 10)
-some_reviewer.rate_hw(best_student, 'Git', 10)
-some_reviewer.rate_hw(best_student, 'Python', 8)
-some_reviewer.rate_hw(best_student, 'Python', 10)
-some_reviewer.rate_hw(best_student, 'Python', 10)
+reviewer2 = Reviewer('Алёна', 'Батицкая')
+reviewer2.courses_attached += ['Python']
+reviewer2.courses_attached += ['Git']
+
+# Оценки Студенту от Рецензера
+reviewer1.rate_hw(student1, 'Python', 7)
+reviewer1.rate_hw(student1, 'Git', 5)
+reviewer1.rate_hw(student1, 'Git', 8)
+reviewer1.rate_hw(student1, 'Python', 3)
+reviewer1.rate_hw(student1, 'Python', 4)
+reviewer1.rate_hw(student2, 'Python', 8)
+reviewer1.rate_hw(student2, 'Git', 6)
+reviewer1.rate_hw(student2, 'Git', 8)
+reviewer1.rate_hw(student2, 'Python', 3)
+reviewer1.rate_hw(student2, 'Python', 5)
+
+reviewer2.rate_hw(student1, 'Python', 6)
+reviewer2.rate_hw(student1, 'Git', 8)
+reviewer2.rate_hw(student1, 'Git', 4)
+reviewer2.rate_hw(student1, 'Python', 5)
+reviewer2.rate_hw(student1, 'Python', 4)
+reviewer2.rate_hw(student2, 'Python', 9)
+reviewer2.rate_hw(student2, 'Git', 6)
+reviewer2.rate_hw(student2, 'Git', 7)
+reviewer2.rate_hw(student2, 'Python', 5)
+reviewer2.rate_hw(student2, 'Python', 9)
 
 # Подсчет средней Оценки у Студента
-best_student.average_score_student(best_student)
-# Подсчет средней Оценки у Лектора
-some_lecturer.average_score_lector(some_lecturer)
+student1.average_score_student(student1)
+student2.average_score_student(student2)
 
-print(best_student)
-print(some_lecturer)
-print(some_reviewer)
+# Подсчет средней Оценки у Лектора
+lecturer1.average_score_lector(lecturer1)
+lecturer2.average_score_lector(lecturer2)
+
+print(student1)
+print(student2)
+print(lecturer1)
+print(lecturer2)
+print(reviewer1)
+print(reviewer2)
+
+print(student1.__lt__(student2))
+print(lecturer2.__lt__(lecturer1))
